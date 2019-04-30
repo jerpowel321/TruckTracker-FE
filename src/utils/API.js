92e3559e39475a9b1bcdfd692dd3d2be55b84f9f
Mel-Marsollier <mmarsollier@outlook.com>
2019-04-30 13:29:10 -0700

    add gh-pages for FE build

---
+++ src/utils/API.js
@@ -0,0 +1,23 @@
+
+import axios from "axios";
+// const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=";
+
+// export default {
+//   // Queries Google Books API
+//     searchBooks: function(query) {
+//         return axios.get(BASEURL + query );
+//     },
+//     getAllTrucks(){
+//         return axios.get(BASEURL + "/api/trucks")
+//     },
+//     saveBook(book){
+//         return axios.post("/api/books/saved", book)
+//     },
+//     getBook: function (id){
+//         return axios.get("/api/book/:id")
+//     },
+//     deleteBook: function (title){
+//         return axios.delete("/api/books/", {data: {name: title} })
+//     },
+   
+// };
