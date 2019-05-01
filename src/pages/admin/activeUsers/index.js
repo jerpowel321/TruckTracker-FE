028c68aaf04c85d1a50f0e2521be2f70e4d04c5a
Jennifer Powell <jerpowel321@gmail.com>
2019-04-30 17:32:54 -0700

    update food truck application

--- src/pages/admin/activeUsers/index.js
+++ src/pages/admin/activeUsers/index.js
@@ -15,20 +15,88 @@ class Admin extends Component {
   //     )
   //     .catch(err => console.log(err));
   // };
-    render() {
-      return (
-        <div className="brickBackground">
+  render() {
+    return (
+      <div className="brickBackground">
 
-          <Nav 
+        <Nav
           currentPage="Active Users"
-          />
-          <Container>
-          <h1>Whats UP WHYYYY CAIN hello hello hello hello</h1>
- 
-          </Container>
-        </div>)
-    }
-  
-  };
-  
-  export default Admin;
+        />
+      
+          <div className="truckApplication">
+          <form>
+            <div class="form-row">
+              <h1>Food Truck Application 2019</h1>
+            </div>
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="companyName">Company Name</label>
+                <input type="text" class="form-control" id="companyName" placeholder="Koja Kitchen" />
+              </div>
+              <div class="form-group col-md-6">
+                <label for="website">Website</label>
+                <input type="text" class="form-control" id="inputMiddleName" placeholder="" />
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="form-group col-md-4">
+                <label for="inputFirstName"> Contact First Name</label>
+                <input type="text" class="form-control" id="inputFirstName" placeholder="William" />
+              </div>
+              <div class="form-group col-md-3">
+                <label for="inputMiddleName">Middle Initial</label>
+                <input type="text" class="form-control" id="inputMiddleName" placeholder="B." />
+              </div>
+              <div class="form-group col-md-4">
+                <label for="inputLastName">Last Name</label>
+                <input type="password" class="form-control" id="inputLastName" placeholder="Pitt" />
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="inputEmail4">Email</label>
+                <input type="email" class="form-control" id="inputEmail4" placeholder="Email" />
+              </div>
+              <div class="form-group col-md-6">
+                <label for="inputPhoneNumber">Phone Number</label>
+                <input type="number" class="form-control" id="inputPhoneNumber" placeholder="4155555" />
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="inputAddress">Address</label>
+              <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" />
+            </div>
+            <div class="form-group">
+              <label for="inputAddress2">Address 2</label>
+              <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
+            </div>
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="inputCity">City</label>
+                <input type="text" class="form-control" id="inputCity" />
+              </div>
+              <div class="form-group col-md-4">
+                <label for="inputState">State</label>
+                <select id="inputState" class="form-control" >
+                  <option selected>Choose...</option>
+                  <option>CA</option>
+                </select>
+              </div>
+              <div class="form-group col-md-2">
+                <label for="inputZip">Zip</label>
+                <input type="text" class="form-control" id="inputZip" />
+              </div>
+            </div>
+            <div class="text-center">
+            <button type="submit" class="btn redBg text-white">Submit</button>
+            </div>
+          </form>
+          </div>
+       
+      </div>
+    )
+  }
+
+};
+
+export default Admin;
