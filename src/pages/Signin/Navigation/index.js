a62579253633c1e4c33a9324fa46cb8ce73d9608
Mel-Marsollier <mmarsollier@outlook.com>
2019-05-02 15:29:31 -0700

    fixing navbar for signin

--- src/pages/Signin/Navigation/index.js
+++ src/pages/Signin/Navigation/index.js
@@ -10,35 +10,44 @@ import { AuthUserContext } from '../Session';
 
 const Navigation = () => (
   <div>
-  <AuthUserContext.Consumer>
-    {authUser =>
-      authUser ? <NavigationAuth /> : <NavigationNonAuth />
-    }
-  </AuthUserContext.Consumer>
-</div>
+    <AuthUserContext.Consumer>
+      {authUser =>
+        authUser ? <NavigationAuth /> : <NavigationNonAuth />
+      }
+    </AuthUserContext.Consumer>
+  </div>
 );
 
 const NavigationAuth = () => (
-  <ul>
-
-    <li>
-      <Link to={ROUTES.HOME}>Home</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.ACCOUNT}>Account</Link>
-    </li>
-    <li>
-      <SignOutButton />
-    </li>
-  </ul>
+  <nav class="navbar navbar-expand-lg navbar-light goldBg redText">
+    <a class="navbar-brand font redText mr-5">Logo</a>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+          <Link to={ROUTES.HOME}>Home</Link>
+        </li>
+        <li class="nav-item active">
+          <Link to={ROUTES.ACCOUNT}>Account</Link>
+        </li>
+        <li class="nav-item active">
+          <SignOutButton />
+        </li>
+      </ul>
+    </div>
+  </nav>
 );
 
 const NavigationNonAuth = () => (
-  <ul>
 
-    {/* <li>
-      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-    </li> */}
-  </ul>
+  <nav class="navbar navbar-expand-lg navbar-light goldBg redText">
+    <a class="navbar-brand font redText mr-5">Logo</a>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+          <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
 );
 export default Navigation;
