7eeb0188c86da609debc8fe11b898ad36a7cab8d
Mel-Marsollier <mmarsollier@outlook.com>
2019-05-05 18:28:46 -0700

    Admin role is up

--- src/pages/Signin/Session/withAuthentication.js
+++ src/pages/Signin/Session/withAuthentication.js
@@ -5,20 +5,18 @@ import { withFirebase } from '../Firebase';
 
 const withAuthentication = Component => {
   class WithAuthentication extends React.Component {
-    constructor(props) {
-      super(props);
 
-      this.state = {
-        authUser: null,
-      };
-    }
-
+    state = {
+      authUser : null
+      } 
+    
     componentDidMount() {
-      this.listener = this.props.firebase.auth.onAuthStateChanged(
+      this.listener = this.props.firebase.onAuthUserListener(
         authUser => {
-          authUser
-            ? this.setState({ authUser })
-            : this.setState({ authUser: null });
+          this.setState({ authUser });
+        },
+        () => {
+          this.setState({ authUser: null });
         },
       );
     }
--- src/pages/Signin/Session/withAuthorization.js
+++ src/pages/Signin/Session/withAuthorization.js
@@ -9,12 +9,13 @@ import * as ROUTES from '../../../constants/routes';
 const withAuthorization = condition => Component => {
   class WithAuthorization extends React.Component {
     componentDidMount() {
-      this.listener = this.props.firebase.auth.onAuthStateChanged(
+      this.listener = this.props.firebase.onAuthUserListener(
         authUser => {
           if (!condition(authUser)) {
             this.props.history.push(ROUTES.SIGN_IN);
           }
         },
+        () => this.props.history.push(ROUTES.SIGN_IN),
       );
     }
 
@@ -25,10 +26,10 @@ const withAuthorization = condition => Component => {
     render() {
       return (
         <AuthUserContext.Consumer>
-        {authUser =>
-          condition(authUser) ? <Component {...this.props} /> : null
-        }
-      </AuthUserContext.Consumer>
+          {authUser =>
+            condition(authUser) ? <Component {...this.props} /> : null
+          }
+        </AuthUserContext.Consumer>
       );
     }
   }
