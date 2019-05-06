8d44d6320ce3eb7eea24966643a8823be3e1c932
ctghadiri <ctghadiri@gmail.com>
2019-05-06 09:39:42 -0700

    Updates

---
+++ precache-manifest.be65c725e7133c946e6012406b15e5f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54245c788881cbf978218937a61ec34f",
+    "url": "/WhatTheTruck-FE/index.html"
+  },
+  {
+    "revision": "20e2647389f1c2478494",
+    "url": "/WhatTheTruck-FE/static/css/main.f919ba60.chunk.css"
+  },
+  {
+    "revision": "929718addc260e88a1ff",
+    "url": "/WhatTheTruck-FE/static/js/2.9d264279.chunk.js"
+  },
+  {
+    "revision": "20e2647389f1c2478494",
+    "url": "/WhatTheTruck-FE/static/js/main.19ad6b63.chunk.js"
+  },
+  {
+    "revision": "69201d97f970a13e182e",
+    "url": "/WhatTheTruck-FE/static/js/runtime~main.5041ae11.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WhatTheTruck-FE/static/media/logo.5d5d9eef.svg"
+  }
+]);
